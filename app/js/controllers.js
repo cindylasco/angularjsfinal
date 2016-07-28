19bd39bf05bda4d2a70dfbd2e35af255d262b014
Cindy Lasco <cindylasco27@gmail.com>
2016-07-28 15:55:16 +0800

    seventeenth commit Steps: Part 6

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -13,3 +13,10 @@ phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams',
   function($scope, $routeParams) {
     $scope.phoneId = $routeParams.phoneId;
   }]);
+  
+  phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', '$http',
+  function($scope, $routeParams, $http) {
+    $http.get('phones/' + $routeParams.phoneId + '.json').success(function(data) {
+      $scope.phone = data;
+    });
+  }]);
