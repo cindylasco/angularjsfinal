c97ba18cc1b64136f6a15ba15eb9ba91d2c83e7a
Ryann Pilapil <ryannpilapil@gmail.com>
2016-07-30 10:30:49 +0800

    22nd Commit

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -20,3 +20,16 @@ phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams',
       $scope.phone = data;
     });
   }]);
+
+  phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', '$http',
+  function($scope, $routeParams, $http) {
+    $http.get('phones/' + $routeParams.phoneId + '.json').success(function(data) {
+      $scope.phone = data;
+      $scope.mainImageUrl = data.images[0];
+    });
+
+    $scope.setImage = function(imageUrl) {
+      $scope.mainImageUrl = imageUrl;
+    }
+  }]);
+
